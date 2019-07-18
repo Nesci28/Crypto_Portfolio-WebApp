51f3d76c020d44319a19bc4bff5a7409a8e0bec0
nesci <markgagnon28@gmail.com>
2019-07-18 15:59:34 -0400

    first push

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const monk = require("monk");
+const path = require("path");
+
+require("dotenv").config();
+
+// Connect to MongoDB
+const db = monk(
+  `${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`
+);
+const balanceDB = db.get(`${process.env.DB_COLLECTION}`);
+
+const app = express();
+
+// Logger
+app.use(morgan("tiny"));
+
+// Cors
+app.use(cors());
+
+// Express body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/v1/db", async function(req, res, next) {
+  const balance = await balanceDB.findOne({ _id: 1 });
+  res.json(balance);
+});
+
+app.use("/", async function(req, res, next) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ js/main.js
@@ -0,0 +1,59 @@
+const url = "https://getbalance-nos.herokuapp.com/api/v1/db";
+
+const h1Date = document.querySelector("#title");
+h1Date.innerHTML =
+  new Date().getHours() +
+  ":" +
+  new Date().getHours() +
+  ":" +
+  new Date().getSeconds() +
+  " - " +
+  new Date().getDate() +
+  "/" +
+  new Date().getMonth() +
+  "/" +
+  new Date().getFullYear();
+
+fetch(url)
+  .then(resp => resp.json())
+  .then(function(data) {
+    const insideData = data.balance;
+    const container = document.getElementById("container");
+    Object.keys(insideData).forEach(key => {
+      if (typeof data.balance[key] == "object") {
+        console.log(data.balance[key]);
+
+        // Creation of the border box
+        let box = document.createElement("div");
+        box.classList.add("box");
+
+        // Creation of the text inside
+        // Title
+        let ticker = document.createElement("h3");
+        ticker.classList.add("title");
+        ticker.innerHTML = key;
+
+        // Creation of the Grid Div
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        for (let i = 0; i < Object.keys(data.balance[key]).length; i++) {
+          let value = Object.keys(data.balance[key])[i];
+
+          let category = document.createElement("p");
+          category.innerHTML = value + " : ";
+          let result = document.createElement("p");
+          result.innerHTML = data.balance[key][value];
+
+          grid.appendChild(category);
+          grid.appendChild(result);
+        }
+        // Appending the new stuff
+        box.appendChild(ticker);
+        box.appendChild(grid);
+        container.appendChild(box);
+      }
+    }, container);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
